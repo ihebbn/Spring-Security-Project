8fc2a6caedb3c9d5603c6e324fd3075162ff6c83
ihebbn <mohamediheb.benslama@esprit.tn>
2024-05-04 18:11:42 +0100

    Update AuthenticationService.java

--- src/main/java/com/ihebbn/springsecurity/Auth/AuthenticationService.java
+++ src/main/java/com/ihebbn/springsecurity/Auth/AuthenticationService.java
@@ -1,8 +1,10 @@
 package com.ihebbn.springsecurity.Auth;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class AuthenticationService {
     public void register(RegistrationRequest request) {
     }
